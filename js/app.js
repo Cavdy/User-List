abdd271cce13b9bcc6eb478668969e5fcab1ed37
Cavdy <cavdyofficials@gmail.com>
2018-09-04 21:19:33 +0100

    updated the app.js file to loop from LS correctly

--- js/app.js
+++ js/app.js
@@ -29,54 +29,64 @@ function loadAllEventListenners() {
 }
 
 // Get Users from LS
-function getUsers(fname, lname, regDate) {
-    let users;
-    if(localStorage.getItem('users') === null) {
-        users = [];
+function getUsers(user) {
+    let fName;
+    let lName;
+    let reg_Date;
+    if(localStorage.getItem('fname') === null && localStorage.getItem('lname') === null && localStorage.getItem('regDate') === null) {
+        fName = [];
+        lName = [];
+        reg_Date = [];
     } else {
-        users = JSON.parse(localStorage.getItem('users'));
+        fName = JSON.parse(localStorage.getItem('fname', 'lname', 'regDate'));
+        lName = JSON.parse(localStorage.getItem('lname'));
+        reg_Date = JSON.parse(localStorage.getItem('regDate'));
     }
 
-    users.forEach(function(fname, lname, regDate){
-        // Create table body(tbody) element
-        const tbody = document.createElement('tbody');
-        // Create table row(tr) element
-        const tr = document.createElement('tr')
-        // Create table cell(td) element
-        const td = document.createElement('td');
-        // add text content and append child
-        td.appendChild(document.createTextNode(fname));
-        // Create table cell(td)2 element
-        const td2 = document.createElement('td');
-        td2.appendChild(document.createTextNode(lname));
-        // Create table cell(td)3 element
-        const td3 = document.createElement('td');
-        td3.appendChild(document.createTextNode(regDate));
-        // Create table cell(td)4 element
-        const td4 = document.createElement('td');
-        // Create Link
-        const link = document.createElement('a');
-        // Add Class
-        link.className = 'delete-item';
-        // Add icon
-        link.innerHTML = '<i class="fa fa-remove"></i>';
-        // Append Link
-        td4.appendChild(link);
+    fName.forEach(function(fname){
+        lName.forEach(function(lname){
+            reg_Date.forEach(function(regDate){
+                // Create table body(tbody) element
+                const tbody = document.createElement('tbody');
+                // Create table row(tr) element
+                const tr = document.createElement('tr')
+                // Create table cell(td) element
+                const td = document.createElement('td');
+                // add text content and append child
+                td.appendChild(document.createTextNode(fname));
+                // Create table cell(td)2 element
+                const td2 = document.createElement('td');
+                td2.appendChild(document.createTextNode(lname));
+                // Create table cell(td)3 element
+                const td3 = document.createElement('td');
+                td3.appendChild(document.createTextNode(regDate));
+                // Create table cell(td)4 element
+                const td4 = document.createElement('td');
+                // Create Link
+                const link = document.createElement('a');
+                // Add Class
+                link.className = 'delete-item';
+                // Add icon
+                link.innerHTML = '<i class="fa fa-remove"></i>';
+                // Append Link
+                td4.appendChild(link);
 
-        // Append td to tr
-        tr.appendChild(td);
-        // Append td2 to tr
-        tr.appendChild(td2);
-        // Append td3 to tr
-        tr.appendChild(td3);
-        // Append td4 to tr
-        tr.appendChild(td4);
+                // Append td to tr
+                tr.appendChild(td);
+                // Append td2 to tr
+                tr.appendChild(td2);
+                // Append td3 to tr
+                tr.appendChild(td3);
+                // Append td4 to tr
+                tr.appendChild(td4);
 
-        // Append Child (tr) to (tbody)
-        tbody.appendChild(tr);
+                // Append Child (tr) to (tbody)
+                tbody.appendChild(tr);
 
-        // Append Child (tbody) to (table)
-        userList.appendChild(tbody);
+                // Append Child (tbody) to (table)
+                userList.appendChild(tbody);
+            });
+        });
     });
 }
 
@@ -141,15 +151,26 @@ function addUsers(e) {
 // Store in local storage function
 function storeInLocalStorage(fname, lname, regDate) {
     
-    let users;
-    if(localStorage.getItem('users') === null) {
-        users = [];
+    let fName;
+    let lName;
+    let reg_Date;
+    if(localStorage.getItem('fname') === null && localStorage.getItem('lname') === null && localStorage.getItem('regDate') === null) {
+        fName = [];
+        lName = [];
+        reg_Date = [];
     } else {
-        users = JSON.parse(localStorage.getItem('users'));
+        fName = JSON.parse(localStorage.getItem('fname'));
+        lName = JSON.parse(localStorage.getItem('lname'));
+        reg_Date = JSON.parse(localStorage.getItem('regDate'));
     }
 
-    users.push(fname, lname, regDate);
-    localStorage.setItem('users', JSON.stringify(users));
+    fName.push(fname);
+    lName.push(lname);
+    reg_Date.push(regDate);
+    
+    localStorage.setItem('fname', JSON.stringify(fName));
+    localStorage.setItem('lname', JSON.stringify(lName));
+    localStorage.setItem('regDate', JSON.stringify(reg_Date));
 }
 
 // Remove Users function
